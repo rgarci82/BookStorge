7ee4b2a4c3e88006b7fafa9ddd263264ed01d953
rgarci82 <ruben.garcia30102@gmail.com>
2024-01-14 02:26:25 -0600

    create book modal

---
+++ frontend/src/components/BookModal.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { BiUserCircle } from "react-icons/bi";
+import { PiBookOpenTextLight } from "react-icons/pi";
+
+function BookModal({ book, onClose }) {
+  return (
+    <div
+      className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
+      onClick={onClose}
+    >
+      <div className="w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative">
+        <AiOutlineClose
+          className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
+          onClick={onClose}
+        />
+        <h2 className="w-fit px-4 py-1 bg-red-300 rounded-lg">
+          {book.publishedYear}
+        </h2>
+        <h4 className="my-2 text-gray-500">{book._id}</h4>
+        <div className="flex justify-start items-center gap-x-2">
+          <PiBookOpenTextLight className="text-red-300 text-2xl" />
+          <h2 className="my-1">{book.title}</h2>
+        </div>
+        <div className="flex justify-start items-center gap-x-2">
+          <BiUserCircle className="text-red-300 text-2xl" />
+          <h2 className="my-1">{book.author}</h2>
+        </div>
+        <p className="mt-4">Description</p>
+        <p className="mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+          quos delectus voluptates esse aliquam explicabo itaque ducimus,
+          laborum ullam, rerum fugiat, recusandae aperiam velit voluptas.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default BookModal;
