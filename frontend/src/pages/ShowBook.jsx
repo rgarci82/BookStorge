c1a87972cd9a45973cbe020163867ba82cce38e9
rgarci82 <105620909+rgarci82@users.noreply.github.com>
2024-01-15 16:36:44 -0600

    styled show book page

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { BsArrowLeft } from "react-icons/bs";
 import { Link, useParams } from "react-router-dom";
 
 function ShowBook() {
@@ -37,14 +38,38 @@ function ShowBook() {
   }, []);
 
   return (
-    <div>
-      <Link to={"/"}>Home</Link>
-      <div>
-        <div>Book Title: {book.title}</div>
-        <div>Book Author: {book.author}</div>
-        <div>Book Publish Year{book.publishedYear}</div>
-        <div>Created At: {createdAtFormatted}</div>
-        <div>Updated At:{updatedAtFormatted}</div>
+    <div className="p-4">
+      <div className="flex">
+        <Link className="bg-sky-800 text-white px-4 py-1 rounded-lg w-fit">
+          <BsArrowLeft className="text-2xl" />
+        </Link>
+      </div>
+      <h1 className="text-3xl my-4">Show Book</h1>
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
+        <div className="my-4">
+          <span className="text-xl mr-4 text-gray-500">Id</span>
+          <span>{book._id}</span>
+        </div>
+        <div className="my-4">
+          <span className="text-xl mr-4 text-gray-500">Title</span>
+          <span>{book.title}</span>
+        </div>
+        <div className="my-4">
+          <span className="text-xl mr-4 text-gray-500">Author</span>
+          <span>{book.author}</span>
+        </div>
+        <div className="my-4">
+          <span className="text-xl mr-4 text-gray-500">Publish Year</span>
+          <span>{book.publishedYear}</span>
+        </div>
+        <div className="my-4">
+          <span className="text-xl mr-4 text-gray-500">Create Time</span>
+          <span>{createdAtFormatted}</span>
+        </div>
+        <div className="my-4">
+          <span className="text-xl mr-4 text-gray-500">Last Update Time</span>
+          <span>{updatedAtFormatted}</span>
+        </div>
       </div>
     </div>
   );
